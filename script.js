68d8fa11e75594ba7817ef70253e57236ecc2a8d
shivendrarox <12queenvictoria12@gmail.com>
2021-01-02 00:17:13 +0530

    fetching data on js file

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("yo");
+
+let _data = {
+    'email': 'mayankmittal@intugine.com'
+   }
+   
+   let fetched_data={};
+   
+   fetch('https://f0ztti2nsk.execute-api.ap-south-1.amazonaws.com/v1/consignment/fetch', { method: 'POST', body: JSON.stringify(_data) , headers: {
+     "Content-type": "application/json; charset=UTF-8",
+   "Authorization": "Bearer " + 'tTU3gFVUdP'} })
+   .then(response => response.json())
+   .then(fetched_data => console.log(fetched_data));
