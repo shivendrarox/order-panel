b701be8a0e1b83df7d7ab2629713e8df9dd55d5c
shivendrarox <12queenvictoria12@gmail.com>
2021-01-02 19:19:48 +0530

    fixed data fetch on js file

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-console.log("yo");
+let user = {
+  email: "mayankmittal@intugine.com",
+};
 
-let _data = {
-    'email': 'mayankmittal@intugine.com'
-   }
-   
-   let fetched_data={};
-   
-   fetch('https://f0ztti2nsk.execute-api.ap-south-1.amazonaws.com/v1/consignment/fetch', { method: 'POST', body: JSON.stringify(_data) , headers: {
-     "Content-type": "application/json; charset=UTF-8",
-   "Authorization": "Bearer " + 'tTU3gFVUdP'} })
-   .then(response => response.json())
-   .then(data => console.log(data));
+async function fetchData() {
+  let response = await fetch(
+    "https://f0ztti2nsk.execute-api.ap-south-1.amazonaws.com/v1/consignment/fetch",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+        Authorization: "Bearer " + "tTU3gFVUdP",
+      },
+      body: JSON.stringify(user),
+    }
+  );
+
+  let result = await response.json();
+  return result;
+}
+
+async function main() {
+  let data = await fetchData();
+  console.log(data);
+}
+
+window.onload = function () {
+  main();
+};
